d13cf048cc4167e278ee101e5da96ff5cb3c81cd
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-09-10 11:55:06 +0200

    Add style for LoginForm

--- src/login/components/LoginForm.tsx
+++ src/login/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IFormField, defaultFormField } from '../../utils/types';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Container, Box, Grid } from '@material-ui/core';
 import { login } from '../../api/methods';
 import history from '../../history';
 
@@ -25,35 +25,52 @@ class LoginForm extends React.Component<{}, LoginFormState> {
   }
 
   render(){
-    return <form onSubmit={(event) => { event.preventDefault(); this.submit() }}>
-      <TextField
-        label="Email"
-        value={this.state.email.value}
-        required={true}
-        onChange={(event) => this.setState({
-          ...this.state,
-          email: {value: event.target.value, isValid: true}
-        })}
-      />
-      <TextField
-        type="password"
-        label="Password"
-        required={true}
-        value={this.state.password.value}
-        onChange={(event) => this.setState({
-          ...this.state,
-          password: {value: event.target.value, isValid: true}
-        })}
-      />
-      <Button
-        color="primary"
-        variant="contained"
-        type="submit"
-      >
-        Submit
-      </Button>
-    </form>
-  }
+    return (
+    <Container maxWidth='xs'>
+      <form onSubmit={(event) => { event.preventDefault(); this.submit() }}>
+        <Box style={{ margin: '2rem 0'}}>
+          <TextField
+            label="Email"
+            value={this.state.email.value}
+            required={true}
+            onChange={(event) => this.setState({
+              ...this.state,
+              email: {value: event.target.value, isValid: true}
+            })}
+            fullWidth={true}
+            style={{margin: '0.5rem 0'}}
+            variant="outlined"
+          />
+          <TextField
+            type="password"
+            label="Password"
+            required={true}
+            value={this.state.password.value}
+            onChange={(event) => this.setState({
+              ...this.state,
+              password: {value: event.target.value, isValid: true}
+            })}
+            fullWidth={true}
+            variant="outlined"
+          />
+        </Box>
+        <Box style={{margin: '1rem 0'}}>
+          <Grid container justify='flex-end'>
+            <Grid item xs={4}>
+              <Button
+                color="primary"
+                variant="contained"
+                type="submit"
+                fullWidth={true}
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </form>
+    </Container>
+  )}
 }
 
 export default LoginForm;
