b35aa4a24aefc1cf86ec488ff5065e73b1432b7d
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-09-10 10:18:55 +0200

    Add Tabs in LoginScreen

--- src/login/components/LoginScreen.tsx
+++ src/login/components/LoginScreen.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Tabs, Tab } from '@material-ui/core';
+import LoginTabPanel from './LoginTabPanel';
 
-class LoginScreen extends React.Component {
+interface LoginScreenState {
+  tab: number;
+}
+
+class LoginScreen extends React.Component<{}, LoginScreenState> {
+  constructor(props: {}){
+    super(props);
+    this.state = { tab: 0 }
+  }
   render(){
-    return <h1>Je suis la page de login</h1>
+    return (
+      <div>
+        <Tabs
+          indicatorColor='primary'
+          textColor='primary'
+          variant="fullWidth"
+          value={this.state.tab}
+          onChange={(_, newTab) => {
+            this.setState({tab: newTab})
+          }}>
+          <Tab label="login" />
+          <Tab label="register" />
+        </Tabs>
+        <LoginTabPanel valueTab={this.state.tab} index={0}>
+          <h1> Salut </h1>
+        </LoginTabPanel>
+        <LoginTabPanel valueTab={this.state.tab} index={1}>
+          <h1> Coucou </h1>
+        </LoginTabPanel>
+      </div>
+    )
   }
 }
 
